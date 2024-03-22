02b090a0071b1fdb3cadaf3c02a27a3f1c70c43f
Paras Parashar <123573626+ParasParashar@users.noreply.github.com>
2024-03-22 12:34:02 +0530

    Update BreadCrumb.jsx

--- components/BreadCrumb.jsx
+++ components/BreadCrumb.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const BreadCrumb = () => {
   return (
-    <div className="absolute w-72 h-8 bottom-[-50px] left-5 md:left-40    breadCrub ">
+    <div className="absolute w-full md:w-72 h-8 bottom-[-50px] left-5 md:left-40    breadCrub ">
       <Link href={"/"}>QuillAudits --&gt; Resources --&gt;</Link>
       <span className="text-[#ffffff]">Case Studies</span>
     </div>
